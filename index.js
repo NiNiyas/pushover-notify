28c25bfcaacac9fcc40646cc8cd669ee7b50c110
Niyas Mohammed <niyasmohammed64@gmail.com>
2022-07-01 12:04:16 +0530

    Added support for schedule event type

--- index.js
+++ index.js
@@ -10,28 +10,35 @@ async function getDefaultDescription() {
     switch (github.context.eventName) {
         case 'push':
             return `<b>${payload.head_commit.committer.name}</b> has pushed <u><i>${context.sha.slice(-7)}</i></u> to  <b>${payload.repository.full_name}</b>.\n\n` +
-                `- <b>Repo</b>: <a href="${payload.repository.html_url}">${payload.repository.full_name}</a>\n` +
-                `- <b>Compare</b>: <a href="${payload.compare}">View Here</a>\n` +
-                `- <b>Author</b>: ${payload.head_commit.author.username}\n` +
-                `- <b>Committer</b>: ${payload.head_commit.committer.name}\n` +
-                `- <b>Pusher</b>: ${payload.pusher.name}\n` +
-                `- <b>Commit URL</b>: <a href="${payload.head_commit.url}/">${context.sha.slice(-7)}</a>\n\n` +
-                `- <b>Commit Message</b>:\n ${payload.head_commit.message}`;
+                `<b>Repo</b>: <a href="${payload.repository.html_url}">${payload.repository.full_name}</a>\n` +
+                `<b>Compare</b>: <a href="${payload.compare}">View Here</a>\n` +
+                `<b>Author</b>: ${payload.head_commit.author.username}\n` +
+                `<b>Committer</b>: ${payload.head_commit.committer.name}\n` +
+                `<b>Pusher</b>: ${payload.pusher.name}\n` +
+                `<b>Commit URL</b>: <a href="${payload.head_commit.url}/">${context.sha.slice(-7)}</a>\n\n` +
+                `<b>Commit Message</b>:\n ${payload.head_commit.message}`;
         case 'release':
             return `<b>${payload.release.author.login}</b> has ${payload.action} <u><i>${payload.release.tag_name}</i></u>.\n\n` +
-                `- <b>Repo</b>: <a href="${payload.repository.html_url}">${payload.repository.full_name}</a>\n` +
-                `- <b>Name</b>: ${payload.release.name}\n` +
-                `- <b>Author</b>: ${payload.release.author.login}\n` +
-                `- <b>Release URL</b>: <a href="${payload.release.html_url}">${payload.release.tag_name}</a>\n` +
-                `- <b>Download Tar</b>: <a href="${payload.release.tarball_url}">Here</a>\n` +
-                `- <b>Download Zip:</b>: <a href="${payload.release.zipball_url}/">Here</a>\n` +
-                `- <b>PreRelease:</b>: ${payload.release.prerelease}\n\n` +
-                `- <b>Release Message</b>:\n ${payload.release.body}`;
+                `<b>Repo</b>: <a href="${payload.repository.html_url}">${payload.repository.full_name}</a>\n` +
+                `<b>Name</b>: ${payload.release.name}\n` +
+                `<b>Author</b>: ${payload.release.author.login}\n` +
+                `<b>Release URL</b>: <a href="${payload.release.html_url}">${payload.release.tag_name}</a>\n` +
+                `<b>Download Tar</b>: <a href="${payload.release.tarball_url}">Here</a>\n` +
+                `<b>Download Zip:</b>: <a href="${payload.release.zipball_url}/">Here</a>\n` +
+                `<b>PreRelease:</b>: ${payload.release.prerelease}\n\n` +
+                `<b>Release Message</b>:\n ${payload.release.body}`;
+        case 'schedule':
+            return `<b>Event:</b> ${context.eventName}\n` +
+                `<b>Repo</b>: <a href="${payload.repository.html_url}">${payload.repository.full_name}</a>\n` +
+                `<b>Ref</b>: ${context.ref}\n` +
+                `<b>Workflow</b>: ${context.workflow}\n` +
+                `<b>Workflow Job Name:</b> ${context.job}\n` +
+                `<b>Commit SHA</b>: ${context.sha}`;
         default:
             return `<u><i>${context.workflow}</i></u> has been run in <a href="${payload.repository.html_url}">${payload.repository.full_name}</a>.\n\n` +
-                `- <b>Workflow Job Name:</b> ${context.job}\n` +
-                `- <b>Workflow Name:</b> ${context.workflow}\n` +
-                `- <b>Repo</b>: <a href="${payload.repository.html_url}">${payload.repository.full_name}</a>`
+                `<b>Workflow Job Name:</b> ${context.job}\n` +
+                `<b>Workflow Name:</b> ${context.workflow}\n` +
+                `<b>Repo</b>: <a href="${payload.repository.html_url}">${payload.repository.full_name}</a>`
     }
 }
 
